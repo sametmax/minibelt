586ecc105a65f2a23d0c7890bc6bdb1001fba8c6
sametmax <lesametlemax@gmail.com>
2015-05-19 13:19:03 +0200

    window iteration now uses tuples by default

--- minibelt.py
+++ minibelt.py
@@ -338,16 +338,35 @@ def window(iterable, size=2, cast=tuple):
         Yields iterms by bunch of a given size, but rolling only one item
         in and out at a time when iterating.
 
+        >>> list(window([1, 2, 3])
+        [(1, 2), (2, 3)]
+
         By default, this will cast the window to a tuple before yielding it;
         however, any function that will accept an iterable as its argument
         is a valid target.
+
+        If you pass None as a cast value, the deque will be returned as-is,
+        which is more performant. However, since only one deque is used
+        for the entire iteration, you'll get the same reference everytime,
+        only the deque will contains different items. The result might not
+        be what you want :
+
+        >>> list(window([1, 2, 3], cast=None))
+        [deque([2, 3], maxlen=2), deque([2, 3], maxlen=2)]
+        
     """
     iterable = iter(iterable)
     d = deque(islice(iterable, size), size)
-    yield cast(d)
-    for x in iterable:
-        d.append(x)
+    if cast:
         yield cast(d)
+        for x in iterable:
+            d.append(x)
+            yield cast(d)
+    else:
+        yield d
+        for x in iterable:
+            d.append(x)
+            yield d
 
 
 def dmerge(d1, d2, merge_func=None):
