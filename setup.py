273de5b0b63d2264ea3cb273340373051dee76c7
sametmax <lesametlemax@gmail.com>
2020-01-24 16:26:30 +0100

    Apply black. Add compat import in setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
+from __future__ import unicode_literals
+
+from io import open
+
 from setuptools import setup
-
-
-open('MANIFEST.in', 'w').write('\n'.join((
-    "include *.rst",
-)))
 
 from minibelt import __version__
 
+open("MANIFEST.in", "w").write("\n".join(("include *.rst",)))
+
 
 setup(
-
     name="minibelt",
     version=__version__,
     author="Sam et Max",
-    py_modules=['minibelt'],
+    py_modules=["minibelt"],
     author_email="lesametlemax@gmail.com",
     description="One-file utility module filled with helper functions for day to day Python programming",
-    long_description=open('README.rst', 'r', encoding="utf-8").read(),
+    long_description=open("README.rst", "r", encoding="utf-8").read(),
     classifiers=[
-        'Programming Language :: Python',
+        "Programming Language :: Python",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: zlib/libpng License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
     ],
-    url="https://github.com/sametmax/minibelt"
+    url="https://github.com/sametmax/minibelt",
 )
